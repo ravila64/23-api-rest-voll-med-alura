08cb7c1fc8775926017163d3a78673ec81e593f3
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2025-06-24 17:35:09 -0500

    first commit voll-med api alura

---
+++ README.md
@@ -0,0 +1,1 @@
+# 23-api-rest-voll-med-alura
---
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -0,0 +1,18 @@
+package med.voll.api;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+	//@Override
+	public void run(String... args) throws Exception {
+		System.out.println("welcome app with spring boot + lombok + mysql + flyway");
+	}
+
+}
