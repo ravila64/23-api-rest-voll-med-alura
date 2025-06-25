9ca2636bf4aa4809cf76e15d60602248a72d0d29
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2025-06-25 18:57:20 -0500

    add Hello ad~nd MedicoController

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -3,12 +3,16 @@ package med.voll.api.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+//MedicoController mc = new MedicoController();
+
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
    @GetMapping
    public String hello(){
+      //System.out.println(mc.registrarMedico());
       return "Hello World !!!";
+
    }
 }
